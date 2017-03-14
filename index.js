c41f8fe9ff674ce11ec6d190cac7d051742380b5
Tamara Bowman <tamara.bowman@accenture.com>
2017-03-13 20:58:42 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Import alexia and create new app
+const alexia = require('alexia');
+const app = alexia.createApp();
+
+/*
+ * Register HelloIntent with one utterance: `Hello`
+ * and static responseText: `Hello from Alexia app`
+ */
+app.intent('HelloIntent', 'Hello', () => {
+    return 'Hello from Alexia app';
+});
+
+// Create http server and start it
+app.createServer().start(() => {
+    // Once started, save speechAssets into directory
+    app.saveSpeechAssets();
+console.log('Server started');
+});
