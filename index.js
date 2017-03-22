92f361f136aea3ad497207a86d68bbec6a6a7fcc
Tamara Bowman <tamara.bowman@accenture.com>
2017-03-22 01:03:45 -0400

    add actions

--- index.js
+++ index.js
@@ -86,6 +86,39 @@ app.intent('DomenicIntent', 'Domenic\'s shoes', () => {
     return 'Where did you get your cool shoes?';
 });
 
+const intentA = app.intent('intentA', 'clear my calendar for {date:Date}', (slots) => {
+    return {
+        text: 'Are you sure you want to clear your calendar?',
+        attrs: {
+            date: slots.date
+        },
+        end: false
+    };
+});
+
+// Or use built-in amazon intents. See: `examples/built-in-intents.js`
+const intentB = app.intent('intentB', 'yes', (slots, attrs) => {
+    // Clear calendar for date `attrs.date` here
+    return 'Your calendar has been cleared';
+});
+
+app.intent('intentC', 'no', () => {
+    return 'Your calendar was not modified';
+});
+
+app.action({
+    from: '*', // Allow transition from any intent to `intent1`. Use '@start' to allow intent only on start
+    to: 'intentA' // Refer to intent by its name or remember its reference. See below
+});
+
+app.action({
+    from: intentA,
+    to: intentB,
+    if: (slots, attrs) => attrs.date, // Note: date should be validated here
+    fail: (slots, attrs) => 'Sorry, your command is invalid'
+});
+
+
 // Create http server and start it
 app.createServer().start(() => {
     // Once started, save speechAssets into directory
